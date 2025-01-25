88f491a34bc329c2b9db177f152046d4046d2fd6
Sakthivel <sakthivel.kss7@gmail.com>
2025-01-25 23:23:21 +0530

    Post edit

--- src/app/posts/postedit/postedit.component.ts
+++ src/app/posts/postedit/postedit.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service'; // Import your service for interacting with the API
 import { ActivatedRoute } from '@angular/router';
 import { Category } from '../types/categorylist.interface';
+import { Status } from '../types/wpstatus.interface';
 @Component({
   selector: 'app-postedit',
   templateUrl: './postedit.component.html',
@@ -11,7 +12,7 @@ import { Category } from '../types/categorylist.interface';
 export class PosteditComponent {
   postForm: FormGroup;
   postId?:number
-  cities: any[] = [];
+  category_list: any[] = [];
   post_status: any[] = []
   categories : Category[] = [];
   constructor(private fb: FormBuilder,private route:ActivatedRoute,private postservice:PostsService) {
@@ -20,53 +21,64 @@ export class PosteditComponent {
       post_content: ['', Validators.required],
       post_id: [''],
       select_status: [''],
+      selectedCategory: ['']
     });
   }
 
 
   ngOnInit(): void {
+     this.postservice.postStatusList().subscribe(data => {
+        this.post_status = data.map(status => ({ name: status.name, code: status.slug }));
+    });
     this.postservice.postCategoryList().subscribe(data=>{
+   
       this.categories=data
     
-      this.cities = this.categories.map(
+      this.category_list = this.categories.map(
         category => 
         (
-          { name: category.name, code: category.slug }
+          { name: category.name, code: category.id }
         )
       );
     })
 
-    this.postservice.postStatusList().subscribe(data => {
-      console.log(data);
-     this.post_status = data.map(status => ({ name: status.name, code: status.slug }));
-      console.log("Post Status");
-      console.log(this.post_status);
-    });
+
  
     this.postId = Number(this.route.snapshot.paramMap.get('id')) || 0;
 
     this.postservice.getSinglePost(this.postId).subscribe(
       {
+
         next: (data:any) => {
+          const selectedCategories = this.category_list.filter(cat =>
+            data.categories?.includes(cat.code)
+          );
+          const sel_stat = this.post_status.find(status => status.code === data.status)
+          console.log(this.post_status)
+          console.log(data.status)
+          console.log(sel_stat)
           this.postForm.patchValue({
             post_title: data?.title?.rendered || '',
             post_content:data?.content?.rendered || '',
-            post_id:data.id
+            post_id:data.id,
+            select_status:this.post_status.find(status => status.name === data.status),
+            selectedCategory:selectedCategories
           });
          
         }
       }
      )
   }
-  onSubmit(): void {
-    if (this.postForm.valid) {
-      const formData = {
-        title: this.postForm.value.post_title,
-        id: this.postForm.value.post_id,
-    }
-    this.postservice.postUpdate(this.postForm.value.post_id,formData).subscribe(data=>{
-      console.log(data);
-    });
+  onSubmit(): void 
+    {
+          if (this.postForm.valid) {
+            const formData = {
+                  title: this.postForm.value.post_title,
+                  id: this.postForm.value.post_id,
+              }
+              this.postservice.postUpdate(this.postForm.value.post_id,formData).subscribe(data=>{
+               
+              });
+        }
   }
-}
 }
